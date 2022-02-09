ae5e3697db319d4001edc6fa4b990cb283b6c59b
Laura <lgomez25@ucm.es>
2022-02-09 12:46:53 +0100

    Creo que ya va bien el proyecto

---
+++ ProyectosIG_x64_VS2019_FG/IG1App/Entity.cpp
@@ -0,0 +1,73 @@
+#include "Entity.h"
+
+#include <gtc/matrix_transform.hpp>  
+#include <gtc/type_ptr.hpp>
+
+using namespace glm;
+
+//-------------------------------------------------------------------------
+
+void Abs_Entity::setColor(dvec4 col)
+{
+	mColor = col;
+}
+
+dvec4 Abs_Entity::getColor()
+{
+	return mColor;
+}
+
+void Abs_Entity::upload(dmat4 const& modelViewMat) const
+{ 
+	glMatrixMode(GL_MODELVIEW);
+	glLoadMatrixd(value_ptr(modelViewMat));  // transfers modelView matrix to the GPU
+}
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+EjesRGB::EjesRGB(GLdouble l): Abs_Entity()
+{
+  mMesh = Mesh::createRGBAxes(l);
+}
+//-------------------------------------------------------------------------
+
+EjesRGB::~EjesRGB() 
+{ 
+	delete mMesh; mMesh = nullptr; 
+};
+//-------------------------------------------------------------------------
+
+void EjesRGB::render(dmat4 const& modelViewMat) const 
+{
+	if (mMesh != nullptr) {
+		dmat4 aMat = modelViewMat * mModelMat;  // glm matrix multiplication
+		upload(aMat);
+		glLineWidth(2);
+		mMesh->render();
+		glLineWidth(1);
+	}
+}
+//-------------------------------------------------------------------------
+
+PoligonsRGB::PoligonsRGB(GLuint num, GLdouble r)
+{
+	mMesh = Mesh::generaPoligonoRegular(num, r);
+}
+
+PoligonsRGB::~PoligonsRGB()
+{
+	delete mMesh; mMesh = nullptr;
+}
+
+void PoligonsRGB::render(glm::dmat4 const& modelViewMat, glm::dvec4 mColor) const
+{
+	if (mMesh != nullptr) {
+		glColor3d(mColor.r, mColor.g, mColor.b);
+		dmat4 aMat = modelViewMat * mModelMat;  // glm matrix multiplication
+		upload(aMat);
+		glLineWidth(2);
+		mMesh->render();
+		glLineWidth(1);
+		glColor3d(0, 0, 0);
+	}
+}
---
+++ ProyectosIG_x64_VS2019_FG/IG1App/Entity.h
@@ -0,0 +1,55 @@
+//#pragma once
+#ifndef _H_Entities_H_
+#define _H_Entities_H_
+
+#include <GL/freeglut.h>
+#include <glm.hpp>
+
+#include "Mesh.h"
+
+//-------------------------------------------------------------------------
+
+class Abs_Entity  // abstract class
+{
+public:
+	Abs_Entity(): mModelMat(1.0), mColor(1) {};  // 4x4 identity matrix
+	virtual ~Abs_Entity() {};
+
+	Abs_Entity(const Abs_Entity& e) = delete;  // no copy constructor
+	Abs_Entity& operator=(const Abs_Entity& e) = delete;  // no copy assignment
+
+	virtual void render(glm::dmat4 const& modelViewMat) const = 0;  // abstract method
+
+	// modeling matrix
+	glm::dmat4 const& modelMat() const { return mModelMat; };
+	void setModelMat(glm::dmat4 const& aMat) { mModelMat = aMat; };
+	
+	void setColor(glm::dvec4 col);
+	glm::dvec4 getColor();
+protected:
+
+	Mesh* mMesh = nullptr;   // the mesh
+	glm::dmat4 mModelMat;    // modeling matrix
+	glm::dvec4 mColor;		//Para los colores
+	// transfers modelViewMat to the GPU
+	virtual void upload(glm::dmat4 const& mModelViewMat) const; 
+};
+//-------------------------------------------------------------------------
+
+class EjesRGB : public Abs_Entity 
+{
+public:
+	explicit EjesRGB(GLdouble l);
+	~EjesRGB();
+	virtual void render(glm::dmat4 const& modelViewMat) const;
+};
+
+//-------------------------------------------------------------------------
+class PoligonsRGB : public Abs_Entity
+{
+public:
+	explicit PoligonsRGB(GLuint num, GLdouble r);
+	~PoligonsRGB();
+	virtual void render(glm::dmat4 const& modelViewMat, glm::dvec4 col) const;
+};
+#endif //_H_Entities_H_
